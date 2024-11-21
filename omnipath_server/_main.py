34d11da7f82466b22606b96d9e11cdf38b4059a9
deeenes <turei.denes@gmail.com>
2024-11-21 14:07:01 +0100

    `main.Runner`: coordinate server loaders and runners

---
+++ omnipath_server/_main.py
@@ -0,0 +1,21 @@
+from .. import _connection
+
+__all__ = [
+    'Runner',
+]
+
+class Runner:
+
+    def __init__(
+            self,
+            con_param: dict | None = None,
+            # TODO: separate load vs. run
+            # TODO: multiple loaders/runners, each with its own config
+            legacy_files: str | dict | None = None,
+    ):
+
+        self.con = _connection.Connection(param = con_param)
+
+    def connect(self):
+
+        self.connection.connect()
