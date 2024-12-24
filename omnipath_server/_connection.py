7c8ec5f7ac3c521a18b135e295ec0b92c337145e
deeenes <turei.denes@gmail.com>
2024-12-24 20:40:12 +0100

    `Connection`: interface to `psycopg2.extras.execute_values`

--- omnipath_server/_connection.py
+++ omnipath_server/_connection.py
@@ -1,17 +1,22 @@
-
-import os
-import yaml
 from contextlib import closing
+import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm  import sessionmaker
+from sqlalchemy.orm import sessionmaker
+import yaml
+import psycopg2.extras
+
+__all__ = [
+    'Connection',
+]
+
 
 class Connection:
     def __init__(
             self,
             param: str | dict | None = None,
-            **kwargs
-        ):
+            **kwargs,
+    ):
 
         self._param = param or kwargs
         self._parse_param()
@@ -26,7 +31,7 @@ class Connection:
 
         if isinstance(self._param, str) and os.path.exists(self._param):
 
-            with closing(open(self._param, 'r')) as fp:
+            with closing(open(self._param)) as fp:
 
                 self._param = yaml.load(fp, Loader = yaml.FullLoader)
 
@@ -49,3 +54,33 @@ class Connection:
 
         self.session.close()
         self.engine.dispose()
+
+
+    def execute_values(
+            self,
+            query: str,
+            values: Generator[tuple, None, None],
+    ) -> None:
+        """
+        Insert by psycopg2.extras.execute_values.
+
+        Args:
+            query:
+                An SQL INSERT query.
+            values:
+                Values to insert.
+        """
+
+        with closing(self.engine.raw_connection()) as conn:
+
+            with closing(conn.cursor()) as cur:
+
+                try:
+
+                    psycopg2.extras.execute_values(cur, query, values)
+                    conn.commit()
+
+                except Exception as e:
+
+                    conn.rollback()
+                    raise e
