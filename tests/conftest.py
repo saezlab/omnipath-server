9a22ea93d0d3cde353e97bfe2cde087698d6c5f3
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-01-15 13:29:00 +0100

    Added legacy path fixture for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 from omnipath_server._connection import Connection
 
-__all__ = ['test_connection']
+__all__ = ['test_connection', 'test_path_legacy']
 
 sys.path.append(str(pl.Path(__file__).parent.parent))
 
@@ -13,3 +13,8 @@ sys.path.append(str(pl.Path(__file__).parent.parent))
 def test_connection():
 
     return Connection('./test_config.yaml')
+
+@pytest.fixture
+def test_path_legacy():
+
+    return './data/legacy/'
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -3,10 +3,12 @@ import pytest
 from omnipath_server.loader._legacy import Loader
 
 
-def create_table():
+def create_table(test_connection, test_path_legacy):
 
-    loader = Loader('./data/legacy/')
-    loader.create() # WIP
+    loader = Loader(path=test_path_legacy, con=test_connection)
+    loader.create()
+
+    # TODO: assert
 
 
 #def test_table():
