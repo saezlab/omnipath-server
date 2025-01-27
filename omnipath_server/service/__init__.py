161507b039f509b251c62939d7ad2b921c4bf73e
deeenes <turei.denes@gmail.com>
2025-01-28 00:49:54 +0100

    `service.__init__`: fixed class name of `LegacyService`

--- omnipath_server/service/__init__.py
+++ omnipath_server/service/__init__.py
@@ -2,7 +2,7 @@
 A service is a class that answers requests from a web API.
 """
 
-from ._legacy import Service as LegacyService
+from ._legacy import LegacyService
 
 __all__ = [
     'LegacyService',
