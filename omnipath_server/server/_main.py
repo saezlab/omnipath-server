47e5ae29e19414075ac990919374f8ba94c35684
deeenes <turei.denes@gmail.com>
2025-01-28 01:35:53 +0100

    `server.main`: outlined main Sanic server

---
+++ omnipath_server/server/_main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+#
+# This file is part of the `omnipath_server` Python module
+#
+# Copyright 2024
+# Heidelberg University Hospital
+#
+# File author(s): OmniPath Team (omnipathdb@gmail.com)
+#
+# Distributed under the GPLv3 license
+# See the file `LICENSE` or read a copy at
+# https://www.gnu.org/licenses/gpl-3.0.txt
+#
+
+from sanic import Sanic, response
+
+__all__ = [
+    'DOMAINS',
+    'SERVERS',
+    'handle_request',
+    'route_requests',
+]
+
+DOMAINS = {
+    'legacy': ('next.omnipathdb.org', 'omnipathdb.org'),
+}
+
+SERVERS = {}
+
+main_server = Sanic('RootServer')
+
+
+@main_server.middleware('request')
+async def route_requests(request):
+
+    request.ctx.server = None
+
+    for service, domains in DOMAINS.items():
+
+        if (
+            any(request.host.startswith(d) for d in domains) and
+            service in SERVERS
+        ):
+
+            request.ctx.server = SERVERS[service]
+
+
+@main_server.route(
+    '/<path:path>',
+    methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+)
+async def handle_request(request, path):
+
+    if (server := request.ctx.server) is not None:
+
+        return await server.handle_request(request)
+
+    return response.text(f'No such service: {request.host}', status = 500)
