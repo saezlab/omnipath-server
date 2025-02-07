2fb88ab1e915b4dc5816feb5b680ee4564f8d8c9
Balazs Bohar <bbohar@ic-5cg3423gzn>
2025-02-07 11:08:36 +0000

    Add parameters for the enzsub example query

--- scripts/legacy_service.py
+++ scripts/legacy_service.py
@@ -13,7 +13,7 @@ service = _legacy.LegacyService(con = con_param)
 
 
 # enz-sub
-req = service.enzsub(limit = 10, format = 'raw')
+req = service.enzsub(enzymes = 'P06239', substrates = 'O14543', limit = 10, format = 'raw')
 
 list(req)
 
