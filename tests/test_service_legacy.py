5e064eedae1374f98874e63d2a1b5c7f00763a61
deeenes <turei.denes@gmail.com>
2025-02-14 14:31:35 +0100

    tests for legacy service where clauses: use pytest parametrize

--- tests/test_service_legacy.py
+++ tests/test_service_legacy.py
@@ -1,7 +1,5 @@
 import pytest
 
-from omnipath_server.service import _legacy
-
 __all__ = [
     'WHERE_CASES',
     'test_where_statement',
@@ -11,24 +9,64 @@ __all__ = [
 WHERE_CASES = {
     'enzsub': [
         (
+            # basic
             {'enzymes': 'P06239', 'substrates': 'O14543', 'limit': 10},
             "(enzsub.ncbi_tax_id = ANY (ARRAY[%(param_1)s])) AND "
             "((enzsub.enzyme = ANY (ARRAY[%(param_2)s])) OR "
-            "(enzsub.enzyme_genesymbol = ANY (ARRAY[%(param_3)s]))) AND "
-            "((enzsub.substrate = ANY (ARRAY[%(param_4)s])) OR "
+            "(enzsub.enzyme_genesymbol = ANY (ARRAY[%(param_3)s])) OR "
+            "(enzsub.substrate = ANY (ARRAY[%(param_4)s])) OR "
             "(enzsub.substrate_genesymbol = ANY (ARRAY[%(param_5)s]))) "
             "LIMIT %(param_6)s",
+        ),
+        (
+            # multiple enzymes
+            {'enzymes': ['P06241', 'P12931'], 'limit': 10},
+            "(enzsub.ncbi_tax_id = ANY (ARRAY[%(param_1)s])) AND "
+            "((enzsub.enzyme = ANY (ARRAY[%(param_2)s, %(param_3)s])) OR "
+            "(enzsub.enzyme_genesymbol = "
+            "ANY (ARRAY[%(param_4)s, %(param_5)s]))) LIMIT %(param_6)s",
+        ),
+        (
+            # enzyme AND substrate (instead of default OR)
+            {
+                'enzymes': 'P06241',
+                'substrates': 'O14543',
+                'enzyme_substrate': 'AND',
+                'limit': 10,
+            },
+            "(enzsub.ncbi_tax_id = ANY (ARRAY[%(param_1)s])) AND "
+            "((enzsub.enzyme = ANY (ARRAY[%(param_2)s])) OR "
+            "(enzsub.enzyme_genesymbol = ANY (ARRAY[%(param_3)s]))) "
+            "AND ((enzsub.substrate = ANY (ARRAY[%(param_4)s])) OR "
+            "(enzsub.substrate_genesymbol = ANY (ARRAY[%(param_5)s]))) "
+            "LIMIT %(param_6)s",
+        ),
+        (
+            {'organisms': 10090},
+            "enzsub.ncbi_tax_id = ANY (ARRAY[%(param_1)s])",
+        ),
+        (
+            # modification types
+            {'types': 'phosphorylation'},
+            "(enzsub.modification = ANY (ARRAY[%(param_1)s])) AND "
+            "(enzsub.ncbi_tax_id = ANY (ARRAY[%(param_2)s]))",
+        ),
+        (
+            # multiple modification types
+            {'types': ['phosphorylation', 'acetylation']},
+            "(enzsub.modification = ANY (ARRAY[%(param_1)s, %(param_2)s])) AND "
+            "(enzsub.ncbi_tax_id = ANY (ARRAY[%(param_3)s]))",
         ),
     ],
 }
 
 
-def test_where_statement(legacy_service):
+@pytest.mark.parametrize(
+    'query_type, args, expected',
+    ((q, a, e) for q, p in WHERE_CASES.items() for a, e in p),
+)
+def test_statements_where(legacy_service, query_type, args, expected):
 
-    for query_type, param in WHERE_CASES.items():
+    stm = legacy_service.query_str(query_type, **args)
 
-        for args, expected in param:
-
-            stm = legacy_service.query_str(query_type, **args)
-
-            assert stm.split('WHERE')[1].strip() == expected
+    assert stm.split('WHERE')[1].strip() == expected
