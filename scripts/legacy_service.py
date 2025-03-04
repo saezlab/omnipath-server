e2b75642e323e8caa8283e737d8fc06577fbb1af
Balazs Bohar <bbohar@ic-5cg3423gzn>
2025-03-04 14:04:28 +0000

    Add example query for dorothea_methods

--- scripts/legacy_service.py
+++ scripts/legacy_service.py
@@ -14,6 +14,7 @@ service = _legacy.LegacyService(con = con_param)
 req = service.interactions(limit=10, datasets=["collectri", 'omnipath'], format='query')
 req = service.interactions(limit=10, directed=True, format='query')
 req = service.interactions(limit=10, entity_types='protein', fields=['entity_type'], organisms=9606, format='query')
+req = service.interactions(limit=10, dorothea_methods='dorothea_curated', format='query')
 str(next(req)[0])
 
 # intercell
