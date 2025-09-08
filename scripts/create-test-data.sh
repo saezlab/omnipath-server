bb3f775303551705541a137afc59dbaeb29550c2
deeenes <turei.denes@gmail.com>
2025-09-08 16:38:21 +0200

    test data export: add header, overwrite files for ech query type; handle gzipped files

--- scripts/create-test-data.sh
+++ scripts/create-test-data.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
+DIRECTORY="data/legacy"
 N_LINES=20
-SAMPLES=(
-    "interactions,post_translational,omnipath;kinaseextra",
+QUERY_TYPES=(
+    "interactions"
+    "enzsub"
+    "intercell"
+    "complexes"
+    "annotations"
 )
+SAMPLES=(
+    "interactions,post_translational,omnipath;kinaseextra"
+)
+
+
+for query_type in ${QUERY_TYPES[@]}; do
+    infile="$DIRECTORY/omnipath_webservice_$query_type.tsv.gz"
+    outfile="$DIRECTORY/$query_type-sample.tsv"
+
+    zcat $infile | head -n 1 > $outfile
+done
 
 for sample in ${SAMPLES[@]}; do
 
     IFS="," read -r query_type filter1 filter2 <<< "$sample"
 
-    INFILE="data/legacy/omnipath_webservice_$query_type.tsv"
-    OUTFILE="data/legacy/sample_$query_type.tsv"
+    infile="$DIRECTORY/omnipath_webservice_$query_type.tsv.gz"
+    outfile="$DIRECTORY/$query_type-sample.tsv"
 
     IFS=';' read -ra datasets <<< "$filter2"
 
     for dataset in "${datasets[@]}"; do
-        grep $filter1 $INFILE | grep $dataset | shuf -n $N_LINES >> $OUTFILE
-        # TODO: >> only appends, make sure to create new one
-        # TODO: add header
+        zcat $infile | grep $filter1 | grep $dataset | shuf -n $N_LINES >> $outfile
     done
 
 done
