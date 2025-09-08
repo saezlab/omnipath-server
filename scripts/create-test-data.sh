a01af0063ca5d0ae9f5489b3f488c7ccad6c6f75
Balazs Bohar <bbazsi41@gmail.com>
2025-09-08 15:02:54 +0100

    Update the test data creator script

--- scripts/create-test-data.sh
+++ scripts/create-test-data.sh
@@ -5,5 +5,11 @@ SAMPLES=(
 )
 
 for sample in ${SAMPLES[@]}; do
+
     IFS="," read -r query_type filter1 filter2 <<< "$sample"
+
+    for word in $filter2; do
+        echo "$query_type, $filter1, $filter2"
+    done
+
 done
