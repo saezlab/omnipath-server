28da7a661770026ba87e7794ec0470d72249be26
deeenes <turei.denes@gmail.com>
2025-09-16 16:00:11 +0200

    test args for the server: more args for `interactions`

--- scripts/r-legacy-server-tests.R
+++ scripts/r-legacy-server-tests.R
@@ -10,13 +10,71 @@ args <- c(
     interactions = list(
         organisms = c(9606, 10090, 10116),
         genesymbols = list('yes', 'no', TRUE, FALSE, 1, 0),
-        datasets = c('omnipath', 'collectri')
+        datasets = c(
+            'omnipath',
+            'collectri',
+            'kinaseextra',
+            'ligrecextra',
+            'pathwayextra',
+            'mirnatarget'
+        ),
+        resources = c(
+            'SIGNOR',
+            'ACSN',
+            'BioGRID',
+            'CellChatDB',
+            'HPRD',
+            'ProtMapper',
+            'PhosphoSite',
+            'CellCall',
+            'ICELLNET',
+            'iTALK',
+            'talklr',
+            'Baccin2019',
+            'EMBRACE',
+            'Wang'
+        ),
+        entity_types = c(
+             'protein',
+             'mirna'
+        ),
+        types = c(
+            'transcriptional',
+            'post_translational'
+        ),
+        license = c(
+            'commercial',
+            'academic'
+        ),
+        loops = c(
+            'no',
+            'yes'
+        ),
+        evidences = c(
+            'no',
+            'yes'
+        ),
+        dorothea_levels = c(
+            NA,
+            'A,B,C'
+        ),
+        dorothea_methods = c(
+            NA,
+            'coexp,tfbs'
+        ),
+        directed = c(
+            'no',
+            'yes'
+        ),
+        signed = c(
+            'no',
+            'yes'
+        )
     ),
     annotations = list(),
     complexes = list(),
     enzsub = list(),
-    intercell = list(),
+    intercell = list()
 )
-
 
 #purrr::cross
