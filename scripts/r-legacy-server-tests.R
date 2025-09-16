0dfe464a0971afd2ef4583a4b2ba5b3836f8c921
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-09-16 15:41:01 +0200

    Working on R script for testing server - collecting argument lists (WIP)

--- scripts/r-legacy-server-tests.R
+++ scripts/r-legacy-server-tests.R
@@ -1,6 +1,22 @@
 #!/usr/bin/env Rscript
 
 library(OmnipathR)
+library(purrr)
 
 options(omnipathr.url = 'http://localhost:44444')
 .optrace()
+
+args <- c(
+    interactions = list(
+        organisms = c(9606, 10090, 10116),
+        genesymbols = list('yes', 'no', TRUE, FALSE, 1, 0),
+        datasets = c('omnipath', 'collectri')
+    ),
+    annotations = list(),
+    complexes = list(),
+    enzsub = list(),
+    intercell = list(),
+)
+
+
+#purrr::cross
